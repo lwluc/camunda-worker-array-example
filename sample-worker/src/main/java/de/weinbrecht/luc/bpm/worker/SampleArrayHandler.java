82f02fd4417f5d9bf34cc92905d0964f751ada1b
Luc Weinbrecht <luc@weinbrecht.de>
2025-01-22 06:17:06 +0100

    initial commit

---
+++ sample-worker/src/main/java/de/weinbrecht/luc/bpm/worker/SampleArrayHandler.java
@@ -0,0 +1,22 @@
+package de.weinbrecht.luc.bpm.worker;
+
+import org.camunda.bpm.client.spring.annotation.ExternalTaskSubscription;
+import org.camunda.bpm.client.task.ExternalTask;
+import org.camunda.bpm.client.task.ExternalTaskHandler;
+import org.camunda.bpm.client.task.ExternalTaskService;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@ExternalTaskSubscription("sampleArrayWorker")
+class SampleArrayHandler implements ExternalTaskHandler {
+
+    @Override
+    public void execute(ExternalTask externalTask, ExternalTaskService externalTaskService) {
+        ArrayList array = new ArrayList<>(List.of("1", "2"));
+        externalTaskService.complete(externalTask, Map.of("sample-array", array));
+    }
+}
---
+++ sample-worker/src/main/java/de/weinbrecht/luc/bpm/worker/WorkerApplication.java
@@ -0,0 +1,13 @@
+package de.weinbrecht.luc.bpm.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerApplication {
+
+  public static void main(String... args) {
+    SpringApplication.run(WorkerApplication.class, args);
+  }
+
+}
